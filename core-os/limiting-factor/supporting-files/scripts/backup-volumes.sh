9c575f0e552ddc66a6865abee278085adcf1a555
Noah Carpenter <noah.carpenter@gmail.com>
2026-02-20 22:17:54 +1100

    added consistency to restore and backup process

--- core-os/limiting-factor/supporting-files/scripts/backup-volumes.sh
+++ core-os/limiting-factor/supporting-files/scripts/backup-volumes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 set -e
-rsync -a --delete /var/lib/postgres/ /var/mnt/nas/docker-volumes/postgres/
-rsync -a --delete /var/lib/ferretdb/ /var/mnt/nas/docker-volumes/ferretdb/
-rsync -a --delete /var/lib/komodo-backups/ /var/mnt/nas/docker-volumes/komodo/
+rsync -a --delete --no-specials --no-links /var/lib/postgres/ /var/mnt/nas/docker-volumes/postgres/
+rsync -a --delete --no-specials --no-links /var/lib/ferretdb/ /var/mnt/nas/docker-volumes/ferretdb/
+rsync -a --delete --no-specials --no-links /var/lib/komodo-backups/ /var/mnt/nas/docker-volumes/komodo/
--- core-os/limiting-factor/supporting-files/scripts/restore-volumes.sh
+++ core-os/limiting-factor/supporting-files/scripts/restore-volumes.sh
@@ -2,11 +2,12 @@
 set -e
 #Only restore if local dirs are empty
 if [ ! "$(ls -A /var/lib/postgres)" ]; then
-    rsync -a /var/mnt/nas-backups/docker-volumes/postgres/ /var/lib/postgres/ || true
+    rsync -a /var/mnt/nas/docker-volumes/postgres/ /var/lib/postgres/ || true
 fi
 if [ ! "$(ls -A /var/lib/ferretdb)" ]; then
-    rsync -a /var/mnt/nas-backups/docker-volumes/ferretdb/ /var/lib/ferretdb/ || true
+    rsync -a /var/mnt/nas/docker-volumes/ferretdb/ /var/lib/ferretdb/ || true
+    chown -R core:etcd /var/lib/ferretdb/
 fi
 if [ ! "$(ls -A /var/lib/komodo-backups)" ]; then
-    rsync -a /var/mnt/nas-backups/docker-volumes/komodo/ /var/lib/komodo-backups/ || true
+    rsync -a /var/mnt/nas/docker-volumes/komodo/ /var/lib/komodo-backups/ || true
 fi
