e9c3edc25299dba0718b2efe974d6adaf868f3da
Noah Carpenter <noah.carpenter@gmail.com>
2026-02-20 20:58:19 +1100

    updated restore functionality to only run once per build from bare-metal

--- core-os/limiting-factor/supporting-files/scripts/restore-media-retrieval.sh
+++ core-os/limiting-factor/supporting-files/scripts/restore-media-retrieval.sh
@@ -7,3 +7,8 @@ rsync -a /var/mnt/nas/docker-volumes/flaresolverr/ /var/lib/media-retrieval/flar
 rsync -a /var/mnt/nas/docker-volumes/sonarr/      /var/lib/media-retrieval/sonarr/
 rsync -a /var/mnt/nas/docker-volumes/radarr/      /var/lib/media-retrieval/radarr/
 rsync -a /var/mnt/nas/docker-volumes/jellyseer/   /var/lib/media-retrieval/jellyseer/
+
+# Fix Jellyseer ownership
+chown -R core:etcd /var/lib/media-retrieval/jellyseer
+# Fix Flaresolverr ownership (if needed)
+chown -R core:etcd /var/lib/media-retrieval/flaresolverr
